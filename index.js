cb4682942d53125141a26e706d01c7e3aeee0cd7
WilliamSosa895 <zs23004708@estudiantes.uv.mx>
2025-04-26 22:23:46 -0600

    corregir index

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 const server = require("./server");
 const PORT = process.env.PORT || 8080;
 
-server.listen(PORT, "0.0.0.0", () =>
-  console.log(`Server is live at http://0.0.0.0:${PORT}`)
+server.listen(PORT, () =>
+  console.log(`Server is live at http://localhost:${PORT}`)
 );
 
 
